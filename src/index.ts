1f1d4da1c5d86f0c5549a0979d4710344145ee8d
Zapic <kawaiizapic@zapic.moe>
2026-01-17 19:56:43 +0800

    improve: increase request read buffer to 4096

--- src/index.ts
+++ src/index.ts
@@ -27,9 +27,17 @@ class H3ServerRequest extends Request {
 const AllowedMethods: HTTPMethod[] = ["CONNECT", "DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT", "TRACE"];
 const encoder = new TextEncoder();
 
+const rCode = '\r'.charCodeAt(0);
+const nCode = '\n'.charCodeAt(0);
+
 const checkEnding = (buf: Uint8Array, size: number) => {
-  if (size < 4) return false;
-  return buf[size - 4] === 13 && buf[size - 3] === 10 && buf[size - 2] === 13 && buf[size - 1] === 10;
+  if (size < 4) return -1;
+  for (let i = size; i > 0; i--) {
+    if (buf[i - 4] === rCode && buf[i - 3] === nCode && buf[i - 2] === rCode && buf[i - 1] === nCode) {
+      return i;
+    }
+  }
+  return -1;
 }
 
 const buildHeader = (headers: Headers): string => {
@@ -42,7 +50,7 @@ const buildHeader = (headers: Headers): string => {
   return result;
 }
 
-const createResponse = (code: number, msg: unknown)=> {
+const createResponse = (code: number, msg: unknown) => {
   return encoder.encode(`HTTP/1.1 ${code}\r\nConnection: closed\r\n\r\n` + msg);
 }
 
@@ -51,30 +59,30 @@ const handleRequest = async (conn: tjs.Connection, handler: RequestHandler, ctx:
     const headerBuf = new Uint8Array(ctx.maxHeaderSize);
     const decoder = new TextDecoder();
     let readSize = 0;
+    let hasCheckMethod = false;
+    let headerEndPosition = -1;
     while (true) {
       if (readSize >= ctx.maxHeaderSize) {
         throw new Error("Exceeded max header size " + ctx.maxHeaderSize);
       }
-      const value = new Uint8Array(1)
-      const count = await conn.read(value);
+      const value = new Uint8Array(4096);
+      const count = await conn.read(value) ?? 0;
       headerBuf.set(value, readSize);
-      readSize += 1;
-      if (readSize === 8) {
+      readSize += count;
+      if (!hasCheckMethod && readSize >= 8) {
         const method = decoder.decode(headerBuf);
         if (!AllowedMethods.some(m => {
           return method.startsWith(m + " ");
         })) {
           throw new Error("Method is not allowed or is not a HTTP request");
         }
+        hasCheckMethod = true;
       }
-      let isDone = false;
-      if (value[0] === 10) {
-        isDone = checkEnding(headerBuf, readSize);
-      }
-      if (!isDone && count === 0) {
+      headerEndPosition = checkEnding(headerBuf, readSize);
+      if (headerEndPosition === -1 && count === 0) {
         throw new Error("Connection closed before body received.");
       }
-      if (isDone) {
+      if (headerEndPosition >= 0) {
         break;
       }
     }
@@ -84,24 +92,35 @@ const handleRequest = async (conn: tjs.Connection, handler: RequestHandler, ctx:
       if (
         typeof info.headers["content-type"] === "string"
         && [
-         "application/json", 
-         "application/x-www-form-urlencode",
-         "text/plain"
-        ].some(t => info.headers["content-type"]!.startsWith(t))
-      ) {
+          "application/json",
+          "application/x-www-form-urlencode",
+          "text/plain"
+        ].some(t => info.headers["content-type"]!.startsWith(t))) {
         let length = ctx.maxBodySize;
-        const maybeLength = info.headers["content-length"]
+        const maybeLength = info.headers["content-length"];
         if (typeof maybeLength === "string" && parseInt(maybeLength, 10).toString() === maybeLength) {
           length = parseInt(maybeLength, 10);
           if (length > ctx.maxBodySize) {
             throw new Error("Exceeded max body size " + ctx.maxBodySize);
           }
         }
-        const bodyBuf = new Uint8Array(length);
-        await conn.read(bodyBuf);
-        reqBody = decoder.decode(bodyBuf);
+        const readInHeader = headerBuf.subarray(headerEndPosition, readSize);
+        const bodyBuf = new Uint8Array(Math.max(length, readInHeader.length));
+
+        bodyBuf.set(readInHeader);
+        let bufOffset = readInHeader.length;
+        while (bufOffset < length) {
+          const bodyReadBuf = new Uint8Array(length);
+          const readSize = await conn.read(bodyReadBuf) ?? 0;
+          bodyBuf.set(bodyReadBuf, bufOffset);
+          bufOffset += readSize;
+          if (readSize === 0) {
+            break;
+          }
+        }
+        reqBody = decoder.decode(bodyBuf.subarray(0, length));
       } else {
-        reqBody = conn.readable;
+        throw new Error("Content-Length is required for non-GET requests with body.");
       }
     }
     const req = new H3ServerRequest(new URL(info.url, "http://" + (info.headers?.host || `${ctx.host}:${ctx.port}`)), {
